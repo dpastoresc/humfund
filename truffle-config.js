db5c3c597b96656ec8178eecc5a4da54def24b04
cbrzn <cesarbrazon10@gmail.com>
2019-05-25 11:47:40 +0200

    creando set up de proyecto

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+require('dotenv').config()  // Store environment-specific variable from '.env' to process.env
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "rinkeby.infura.io/v3/371b884c687647b9be1395dc80f7952b",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.NODE_URL),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
+  }
+};
